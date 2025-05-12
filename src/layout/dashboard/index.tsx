6b39b7f37f31d1b65557a75fd4e75d590603d4ef
aiown <aiown@outlook.com>
2025-05-12 21:21:39 +0800

    update

--- src/layout/dashboard/index.tsx
+++ src/layout/dashboard/index.tsx
@@ -3,13 +3,11 @@ import { Link, Outlet } from 'react-router-dom';
 const Layout: React.FC = () => {
   return (
     <div>
-      <nav style={{ marginBottom: '20px', borderBottom: '1px solid #ccc', paddingBottom: '10px' }}>
-        <ul style={{ listStyle: 'none', padding: 0, margin: 0, display: 'flex', gap: '15px' }}>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-          <li><Link to="/test">test</Link></li>
-        </ul>
-      </nav>
+
+
+      
+
+  
       <main>
         <Outlet /> {/* Child routes will render here */}
       </main>
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,3 +1,15 @@
 import React from 'react';
-const Home: React.FC = () => <h2>Home Page</h2>;
+import { Button } from 'antd';
+const Home: React.FC = () => {
+    return (
+        <>
+            {/* <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1> */}
+            <h2>Home Page</h2>
+            <Button type="primary">Button</Button>
+        </>
+    )
+
+}
 export default Home;
